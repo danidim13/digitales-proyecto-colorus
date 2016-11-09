1ed41d1c691ae2149696c4d702ff3de4ca3c519c
Daniel <danidiaza@gmail.com>
2016-11-09 15:37:03 +0000

    Comit inicial copia lab4

---
+++ Codigo/TestBench.v
@@ -0,0 +1,111 @@
+`timescale 1ns / 1ps
+
+////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer:
+//
+// Create Date:   22:30:52 01/30/2011
+// Design Name:   MiniAlu
+// Module Name:   D:/Proyecto/RTL/Dev/MiniALU/TestBench.v
+// Project Name:  MiniALU
+// Target Device:  
+// Tool versions:  
+// Description: 
+//
+// Verilog Test Fixture created by ISE for module: MiniAlu
+//
+// Dependencies:
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+////////////////////////////////////////////////////////////////////////////////
+`include "Defintions.v"
+module TestBench;
+
+	// Inputs
+	reg Clock;
+	reg Reset;
+
+	// Outputs
+	wire [7:0] oLed;
+
+	wire wHS, wVS, vga_red, vga_green, vga_blue;
+	// Instantiate the Unit Under Test (UUT)
+	MiniAlu uut (
+		.Clock(Clock), 
+		.Reset(Reset), 
+		.oLed(oLed),
+		.VGA_HSYNC(wHS) ,
+		.VGA_VSYNC(wVS) ,
+		.VGA_RED(vga_red) ,
+		.VGA_GREEN(vga_green) , 
+		.VGA_BLUE(vga_blue)   
+	);
+
+	/*
+	Bichillo semiCLK(
+		.Clock(Clock),	
+		.Reset(Reset),
+		.VGA_HSYNC(wHS),
+		.VGA_VSYNC(wVS),
+		.VGA_RED(vga_red),
+		.VGA_GREEN(vga_green),
+		.VGA_BLUE(vga_blue)
+	);*/
+/*
+	RAM_SINGLE_READ_PORT # (3,24,640*480) VideoMemory
+	(
+	.Clock( Clock ),
+	.iWriteEnable( EnableALU ),
+	.iReadAddress(wHS*wVS+wHS ),
+	.iWriteAddress( ),
+	.iDataIn( ),
+	.oDataOut({oVGA_R,oVGA_G,oVGA_B})
+	);*/
+
+
+
+/*	VGA_Controller vga (
+		.Clock(Clock),
+		.Enable(1'b1),
+		.Reset(Reset),
+		.iPixel({oVGA_R,oVGA_G,oVGA_B}),
+		.oHorizontalSync(wHS),
+		.oVerticalSync(wVS),
+		.oRed(vga_red),
+		.oGreen(vga_green),
+		.oBlue(vga_blue),
+		.oQHS (wHS)
+		.oQVS (wVS)
+	);*/
+	
+	always
+	begin
+		#5  Clock =  ! Clock;
+
+	end
+
+	initial begin
+		//$dumpfile("signals.vcd");
+		//$dumpvars;
+		// Initialize Inputs
+		Clock = 0;
+		Reset = 0;
+
+		// Wait 100 ns for global reset to finish
+		#100;
+		Reset = 1;
+		#50
+		Reset = 0;
+        
+		// Add stimulus here
+
+		#1800000000
+		$finish;
+
+	end
+      
+endmodule
+
---
+++ Codigo/VGA.v
@@ -0,0 +1,83 @@
+`include "Defintions.v"
+`include "Collaterals.v"
+
+module VGA_Controller (
+	input wire Clock,
+	input wire Enable,
+	input wire Reset,
+	input wire[2:0] iPixel,
+	output wire oHorizontalSync,
+	output wire oVerticalSync,
+	output wire oRed,
+	output wire oGreen,
+	output wire oBlue,
+	output wire [9:0] oColumnCount,
+	output wire[9:0] oRowCount
+);
+
+wire[10:0] wColumnCount;
+wire wColumnReset, wRowReset;
+wire[9:0] wColumnCount_2, wRowCount;
+wire[4:0] wColumProyec;
+wire[3:0] wRowProyec;
+
+assign wColumnCount_2 = {wColumnCount[10:1]};
+
+assign wColumnReset = (wColumnCount == 11'd1599);
+assign wRowReset = (wRowCount == 10'd524 && wColumnReset);
+
+UPCOUNTER_POSEDGE # ( 11 ) COLUMN_COUNTER 
+(
+	.Clock( Clock ),
+	.Reset( wColumnReset | Reset ),
+	.Initial( 11'd0 ),
+	.Enable( 1'b1 ),
+	.Q( wColumnCount )
+);
+
+UPCOUNTER_POSEDGE # ( 10 ) ROW_COUNTER 
+(
+	.Clock( Clock ),
+	.Reset( wRowReset | Reset ),
+	.Initial( 10'd0 ),
+	.Enable( wColumnReset ),
+	.Q( wRowCount )
+);
+
+parameter H_VISIBLE_AREA 	= 640;
+parameter H_FRONT_PORCH 	= 16;
+parameter H_PULSE				= 96;
+parameter H_BACK_PORCH		= 48;
+parameter HORIZONTAL_LINE	= 800;
+
+parameter V_VISIBLE_AREA 	= 480;
+parameter V_FRONT_PORCH 	= 10;
+parameter V_PULSE				= 2;
+parameter V_BACK_PORCH		= 33;
+parameter VERTICAL_LINE		= 525;
+
+
+assign oRowCount = wRowCount;
+assign oColumnCount = wColumnCount_2;
+
+assign oHorizontalSync = 
+(
+		wColumnCount_2 >= (H_VISIBLE_AREA + H_FRONT_PORCH ) &&
+		wColumnCount_2 <= (H_VISIBLE_AREA + H_FRONT_PORCH + H_PULSE )
+) ? 1'b0 : 1'b1;
+
+assign oVerticalSync = 
+(
+		wRowCount >= (V_VISIBLE_AREA + V_FRONT_PORCH ) &&
+		wRowCount <= (V_VISIBLE_AREA + V_FRONT_PORCH + V_PULSE )
+) ? 1'b0 : 1'b1;
+	
+assign {oRed,oGreen,oBlue} = (wColumnCount_2 < H_VISIBLE_AREA && wRowCount < V_VISIBLE_AREA) ?
+ {iPixel} :	//display color
+ {`BLACK}; //black
+
+
+endmodule
+
+
+
