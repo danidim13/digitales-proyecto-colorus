e23c99839afb1c4205221114c0c1b15934560546
Daniel <danidiaza@gmail.com>
2016-11-10 01:34:21 +0000

    Controlador del Sprite

--- Codigo/Sprite_Controller.v
+++ Codigo/Sprite_Controller.v
@@ -5,7 +5,7 @@
 // 
 // Create Date:    09:23:14 11/09/2016 
 // Design Name: 
-// Module Name:    Sprite 
+// Module Name:    Sprite_Controller
 // Project Name: 
 // Target Devices: 
 // Tool versions: 
@@ -18,11 +18,11 @@
 // Additional Comments: 
 //
 //////////////////////////////////////////////////////////////////////////////////
-module Sprite_Controller # ( parameter SizeX= 32, parameter SizeY=32) (
+module Sprite_Controller # ( parameter SizeX = 32, parameter SizeY = 32) (
 
 	input wire [9:0] iColumnCount,
-	input wire[9:0] iRowCount,
-   input wire imask,
+	input wire [9:0] iRowCount,
+	input wire imask,
 	input wire iEnable,
 	input wire [9:0] iPosX,
 	input wire [9:0] iPosY,
@@ -30,19 +30,19 @@ module Sprite_Controller # ( parameter SizeX= 32, parameter SizeY=32) (
 	input wire [2:0] iColorBack ,
 	output reg [2:0] oRGB
 	
-	);
+);
 
 	
 	always @ (*)
+	begin 
+		if(iColumnCount <= SizeX + iPosX && iRowCount <= SizeY + iPosY  
+		&& iColumnCount >= iPosX && iRowCount >= iPosY && iEnable == 1 && imask == 1 ) 
+		begin
+			 oRGB <= iColorSprite;
+		end 
+		else 
 		begin 
-			if(iColumnCount <= SizeX + iPosX && iRowCount <= SizeY + iPosY  
-			&& iColumnCount >= iPosX && iRowCount >= iPosY && iEnable == 1 && imask == 1 ) 
-				begin
-					 oRGB <= iColorSprite;
-				end 
-			else 
-				begin 
-					oRGB <= iColorBack;
-				end	
-		end  
+			oRGB <= iColorBack;
+		end	
+	end  
 endmodule
