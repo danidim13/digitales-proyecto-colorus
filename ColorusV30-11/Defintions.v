c35e7e1adb860357d228a85ed69466bf0982a87d
cvegaj <carlavj93@gmail.com>
2016-12-06 21:13:21 -0600

    Update Defintions.v

--- ColorusV30-11/Defintions.v
+++ ColorusV30-11/Defintions.v
@@ -19,9 +19,9 @@
 `define RET   4'd9	// 1001
 `define WVM  4'd10	// 1001
 `define CHCOLOR  4'd11	// 1001
-`define MOVSP 4'd12
-`define MOVSPR 4'd13
-`define READPOSX 4'd14
+`define MOVSP 4'd1
+`define MOVSPR 4'd12
+`define READPOSX 4'd13
 
 
 
